f690851c65203958528003a5a2e1efd49a9df0a3
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-24 17:24:05 -0300

    Adição de paginação

---
+++ resources/views/components/pagination.blade.php
@@ -0,0 +1,13 @@
+<nav aria-label="Page navigation example" style="bottom: 0">
+    <ul class="pagination justify-content-center">
+      <li class="page-item disabled">
+        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+      </li>
+      <li class="page-item"><a class="page-link" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item">
+        <a class="page-link" href="#">Pŕoxima</a>
+      </li>
+    </ul>
+  </nav>
