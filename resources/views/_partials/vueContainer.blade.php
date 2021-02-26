761a19f3596eb84c6f1528f8b3fef52070474eab
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-26 13:50:33 -0300

    Adição do codigo vue como _partials

---
+++ resources/views/_partials/vueContainer.blade.php
@@ -0,0 +1,14 @@
+<script>
+    let containerFilmes = new Vue({
+        el: '#containerFilmes',
+        data: {
+            containerStyle: {
+                textAlign: 'center',
+                marginTop: '20px',
+                marginLeft: '7%',
+                maginRight: '15%',
+                paddingRight: '0'
+            }
+        }
+    })
+</script>
