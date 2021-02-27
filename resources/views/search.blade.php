b0a933df8241804f6dce6fb9c16dbce0da6c8c73
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-27 14:22:45 -0300

    transferencia de código para imgThumbnail

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,28 +3,17 @@
 @section('title', "busca por ".$query)
 
 @section('content')
-    <div class="container" id="containerFilmes" v-bind:style="containerStyle">
-        <h1>Buscar</h1>
-        <h6>Foram encontrados {{ $result['total_results'] }} resultados na sua busca</h6>
-        @foreach ($result['results'] as $filme)
-            <div class="container-sm">
-                <a href="{{ route('catalogo.show',$filme['id']) }}">
-                    <img style="height: 280px; width:190px; float:left; margin-left: 5px" 
-                    src="https://image.tmdb.org/t/p/w500{{ $filme['poster_path'] }}" 
-                    alt="{{ $filme['original_title'] }}" 
-                    class="img-thumbnail"
-                    data-bs-trigger="hover"
-                    data-bs-toggle="popover" 
-                    title="{{ $filme['title'] }}" 
-                    data-bs-content="{{ mb_strimwidth($filme['overview'],0,500,'...')  }} 
-                        Lançamento: {{ implode("-",array_reverse(explode("-",$filme['release_date'] ?? ''))) }}"
-                        {{-- Genero: {{ $filme['genre_ids'] }}" --}}
-                    >
-                </a>
-            </div>
-        @endforeach
-        
+    <div class="d-flex flex-column bd-highlight mb-3" id="containerFilmes" v-bind:style="containerStyle">
+        <h1 class="p-2 bd-highlight" :style="titleStyle">Buscar</h1>
+        <h6 class="p-2 bd-highlight">Foram encontrados {{ $result['total_results'] }} resultados na sua busca</h6>
+        <div class="container-sm">
+            @foreach ($result['results'] as $filme)
+                @include('_partials.imgThumbnail')
+            @endforeach
+        </div>
+        <div class="p-2 bd-highlight">
+            @include('components.pagination')
+        </div>
     </div>
-    @include('components.pagination')
     @include('_partials.vueContainer')
 @endsection
