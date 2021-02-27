edfde71cbb3f0ca8983f998675cc565d16f809fc
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-27 14:33:54 -0300

    Adição de páginação quase dinâmica

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,13 +1,14 @@
-<nav aria-label="Page navigation example" style="bottom: 0">
+<nav aria-label="Page navigation example" style="">
     <ul class="pagination justify-content-center">
-      <li class="page-item disabled">
-        <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Anterior</a>
+      <li class="page-item ">
+        <a class="page-link" href="{{ route('index',$page-1) ?? '' }}" tabindex="-1" >Anterior</a>
       </li>
-      <li class="page-item"><a class="page-link" href="#">1</a></li>
-      <li class="page-item"><a class="page-link" href="#">2</a></li>
-      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item"><a class="page-link" href="{{ route('index', 1) }}">{{ 1 }}</a></li>
+      <li class="page-item"><a class="page-link" href="{{ route('index', 3) }}">{{ 2 }}</a></li>
+      <li class="page-item"><a class="page-link" href="{{ route('index', 5) }}">{{ 3 }}</a></li>
+      <li class="page-item"><a class="page-link" href="{{ route('index', 7) }}">{{ 4 }}</a></li>
       <li class="page-item">
-        <a class="page-link" href="#">Pŕoxima</a>
+        <a class="page-link" href="{{ route('index',$page+1) ?? '' }}">Próxima</a>
       </li>
     </ul>
   </nav>
