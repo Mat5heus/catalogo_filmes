602fc5f61b6924e6fd5f81a04708f674435fb8f5
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-27 18:55:17 -0300

    Troca de container por flexbox e ajuste de layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,17 +5,17 @@
 @section('content')
     <style>
         html, body {
+            width: 100.5%;
             overflow-x: hidden;
             overflow-y: scroll;
         }
     </style>
-    <div class="d-flex flex-column bd-highlight mb-3 justify-content-center" id="containerFilmes" >
-        {{-- <div class="p-3 mb-2 ">.bg-dark</div> --}}
-        <div class="p-2 bd-highlight bg-dark text-white" :style="carouselStyle">
+    <div class="d-flex flex-column bd-highlight mb-3" id="containerFilmes" >
+        <div class="p-2 bd-highlight bg-dark text-white" :style="carouselContainerStyle">
             @include('components.carousel')
         </div>
         <h1 class="p-2 bd-highlight" :style="titleStyle">Catálogo</h1>
-        <div class="container-sm" :style="containerStyle">
+        <div class="row d-flex justify-content-center" :style="containerStyle">
             @foreach ($lista as $filme)
                 @include('_partials.imgThumbnail')
             @endforeach
