987dd6165eea8bacf436cc5b7d8c05d84ab23661
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-27 14:21:17 -0300

    Adição de flexbox utilities e remoção do scroll-x

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,27 +3,26 @@
 @section('title', "Próximos lançamentos")
 
 @section('content')
-    <div class="container" id="containerFilmes" v-bind:style="containerStyle">
-        <h1>Próximos Lançamentos</h1>
-        @foreach ($lista as $filme)
-            <div class="container-sm">
-                <a href="{{ route('catalogo.show', $filme['id']) }}">
-                    <img style="height: 280px; width:190px; float:left; margin-left: 5px" 
-                        src="https://image.tmdb.org/t/p/w500/{{ $filme['poster_path'] }}" 
-                        alt="{{ $filme['original_title'] }}" 
-                        class="img-thumbnail"
-                        data-bs-trigger="hover"
-                        data-bs-toggle="popover" 
-                        title="{{ $filme['title'] }}" 
-                        data-bs-content="{{ mb_strimwidth($filme['overview'],0,500,'...')  }} 
-                            Lançamento: {{ implode("/",array_reverse(explode("-",$filme['release_date']))) }}"
-                            {{-- Genero: {{ $filme['genre_ids'] }}" --}}
-                    >
-                </a>
-            </div>
-        @endforeach
+    <style>
+        html, body {
+            overflow-x: hidden;
+            overflow-y: scroll;
+        }
+    </style>
+    <div class="d-flex flex-column bd-highlight mb-3 justify-content-center" id="containerFilmes" >
+        {{-- <div class="p-3 mb-2 ">.bg-dark</div> --}}
+        <div class="p-2 bd-highlight bg-dark text-white" :style="carouselStyle">
+            @include('components.carousel')
+        </div>
+        <h1 class="p-2 bd-highlight" :style="titleStyle">Catálogo</h1>
+        <div class="container-sm" :style="containerStyle">
+            @foreach ($lista as $filme)
+                @include('_partials.imgThumbnail')
+            @endforeach
+        </div>
+        <div class="p-2 bd-highlight">
+            @include('components.pagination')
+        </div>
     </div>
-    @include('components.pagination')
     @include('_partials.vueContainer')
-    
 @endsection
