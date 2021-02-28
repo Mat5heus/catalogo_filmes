b9ccdc89c02dba25a032a629d7fcae0b685fcaf8
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-28 14:40:32 -0300

    Redução de código

--- resources/views/_partials/vueContainer.blade.php
+++ resources/views/_partials/vueContainer.blade.php
@@ -2,6 +2,8 @@
     let containerFilmes = new Vue({
         el: '#containerFilmes',
         data: {
+            page: [],
+            jsonName: 'favoritos',
             cardStyle: {
                 height: '280px', 
                 width:'190px', 
@@ -13,15 +15,29 @@
                 paddingRight: '5%'                
             },
             titleStyle: {
+                textAlign: 'center',
+                marginTop: '50px'
+            },
+            subtitleStyle: {
                 textAlign: 'center'
             },
             carouselContainerStyle: {
                 display: 'flex',
-                height: '500px'
+                height: '500px',
+                marginTop: '49px'
             },
             carouselStyle:{
                 marginLeft:'15%',
                 width: '70%'
+            }
+        },
+        mounted() {
+            if (localStorage.getItem(this.jsonName)) {
+                try {
+                    this.page = JSON.parse(localStorage.getItem(this.jsonName));
+                } catch(e) {
+                    console.error(e);
+                }
             }
         },
         methods: {
