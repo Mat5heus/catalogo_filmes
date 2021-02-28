2e83d51cfcb298c107e217644c86d74e2b247e72
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-28 14:22:34 -0300

    remoção de reduncia e separação de titulo e subtítulo

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -5,8 +5,8 @@
 @section('content')
     <div class="d-flex flex-column bd-highlight mb-3" id="containerFilmes" v-bind:style="containerStyle">
         <h1 class="p-2 bd-highlight" :style="titleStyle">Buscar</h1>
-        <h6 class="p-2 bd-highlight" :style="titleStyle">Foram encontrados {{ $result['total_results'] }} resultados na sua busca</h6>
-        <div class="row d-flex justify-content-center" :style="containerStyle">
+        <h6 class="p-2 bd-highlight" :style="subtitleStyle">Foram encontrados {{ $result['total_results'] }} resultados na sua busca</h6>
+        <div class="row d-flex justify-content-center">
             @foreach ($result['results'] as $filme)
                 @include('_partials.imgThumbnail')
             @endforeach
