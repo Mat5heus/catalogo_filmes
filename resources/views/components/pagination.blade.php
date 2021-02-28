94f94cf34d6e072a49b74fe9a9d186f5ffd21934
Mat5heus <matheus.santos28@alunos.fho.edu.br>
2021-02-28 14:42:59 -0300

    Menos uma quebra de linha

--- resources/views/components/pagination.blade.php
+++ resources/views/components/pagination.blade.php
@@ -1,13 +1,14 @@
 <nav aria-label="Page navigation example" style="{{ (($result['total_results'] ?? '') == 0) ? 'visibility:hidden': '' }}">
-    <ul class="pagination justify-content-center">
-      <li class="page-item {{ ($currentPageNumber <= 1) ? 'disabled' : '' }}">
-        <a class="page-link" href="{{ route($currentPageName,$currentPageNumber-3) }}" tabindex="-1" >Anterior</a>
-      </li>
-      <li class="page-item {{ ($currentPageNumber >= $total_pages) ? 'disabled' : '' }}">
-        <a class="page-link" 
-          href="
-            {{ route($currentPageName,$currentPageNumber+1) }}{{ ($currentPageName == 'search') ? '?query='.urlencode($query) : '' }}
-          ">Próxima</a>
-      </li>
-    </ul>
-  </nav>
+  <ul class="pagination justify-content-center">
+    <li class="page-item {{ ($currentPageNumber <= 1) ? 'disabled' : '' }}">
+      <a class="page-link" href="{{ route($currentPageName,$currentPageNumber-(($currentPageName == 'search') ? 1 : 3)) }}{{ ($currentPageName == 'search') ? '?query='.urlencode($query) : '' }}" tabindex="-1" >
+        Anterior
+      </a>
+    </li>
+    <li class="page-item {{ ($currentPageNumber >= $total_pages) ? 'disabled' : '' }}">
+      <a class="page-link" href="{{ route($currentPageName,$currentPageNumber+1) }}{{ ($currentPageName == 'search') ? '?query='.urlencode($query) : '' }}">
+        Próxima
+      </a>
+    </li>
+  </ul>
+</nav>
